8a5d46b7ecc80f5ebc40c3e8a277426628e92b0b
arima <mickeylindberg@yahoo.co.jp>
2020-05-04 04:40:31 +0000

    validation & add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,13 +1,25 @@
 @extends('layouts.app')
 @section('content')
-
 <!-- ページ毎のコンテンツ -->
+<!--@if(count($errors)>0)-->
+<!--    <ul class="alert alert-danger" role='alert'>-->
+<!--        @foreach($errors->all() as $error)-->
+<!--        <li class="ml-4">{{$error}}</li>-->
+<!--        @endforeach-->
+<!--    </ul>-->
+<!--@endif-->
+
 
 <h1>新しいタスクの追加ページ</h1>
 
 <div class="row">
     <div class="col-6">
         {!! Form::model($task,['route' => 'tasks.store']) !!}
+        
+        <div class="form-group">
+            {!! Form::label('status','status:') !!}
+            {!! Form::text('status',null,['class'=>'form-control']) !!}
+        </div>
         
         <div class="form-group">
             {!! Form::label('content' , 'タスク：') !!}
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,11 +2,26 @@
 @section('content')
 
 <!-- ページ毎のコンテンツ -->
+
+<!--@if (count($errors) > 0)-->
+<!--        <ul class="alert alert-danger" role="alert">-->
+<!--            @foreach ($errors->all() as $error)-->
+<!--                <li class="ml-4">{{ $error }}</li>-->
+<!--            @endforeach-->
+<!--        </ul>-->
+<!--@endif-->
+
 <h1>id: {{$task->id}}のタスク編集ページ</h1>
 
 <div class="row">
  <div class="col-6">
     {!! Form::model($task,['route'=>['tasks.update',$task->id],'method'=>'put']) !!}
+    
+    <div class="form-group">
+        {!! Form::label('status', 'status:') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+    </div>
+
     
     <div class="form-group">
         {!! Form::label('content','タスク：') !!}
